8b6a76cfffb49f72fa58f6a2e248277388ab4fcd
emb <>
2011-11-09 22:52:39 +0000

    Added getTriangleVertexNo()

--- nist/pededitor/DiagramType.java
+++ nist/pededitor/DiagramType.java
@@ -13,30 +13,37 @@ public enum DiagramType {
     BINARY ("images/binaryicon.png", "Binary or Cartesian", 4, false),
     TERNARY ("images/triangleicon.png", "Ternary", 3, true),
     TERNARY_LEFT ("images/leftangleicon.png",
-                  "Partial Ternary -- bottom left corner", 3, true),
+                  "Partial Ternary -- bottom left corner", 3, true, 0),
     TERNARY_RIGHT ("images/rightangleicon.png",
-                   "Partial ternary -- bottom right corner", 3, true),
+                   "Partial ternary -- bottom right corner", 3, true, 2),
     TERNARY_TOP ("images/upangleicon.png",
-                 "Partial ternary -- top corner", 3, true),
+                 "Partial ternary -- top corner", 3, true, 1),
     TERNARY_BOTTOM ("images/trianglebottomicon.png",
                     "Partial ternary -- top corner missing", 4, true),
         OTHER ("images/schematicicon.png", "Schematic/Other", 4, false);
 
     private final int mVertexCnt;
     private final URL mIconUrl;
-    private boolean mIsTernary;
+    private final String mDescription;
+    private final int mTriangleVertexNo;
+    private final boolean mIsTernary;
     private Icon mIcon = null;
-    private String mDescription;
 
     DiagramType(String imagePath, String description, int vertexCnt,
-                boolean isTernary) {
+                boolean isTernary, int triangleVertexNo) {
         this.mDescription = description;
         this.mVertexCnt = vertexCnt;
         this.mIsTernary = isTernary;
+        this.mTriangleVertexNo = triangleVertexNo;
         mIconUrl = DiagramType.class.getResource(imagePath);
         if (mIconUrl == null) {
             throw new IllegalStateException("Could not load " + imagePath);
         }
+    }
+
+    DiagramType(String imagePath, String description, int vertexCnt,
+                boolean isTernary) {
+        this(imagePath, description, vertexCnt, isTernary, -1);
     }
 
     public Icon getIcon() {
@@ -60,5 +67,13 @@ public enum DiagramType {
 
     public boolean isTernary() {
         return mIsTernary;
+    }
+
+    /** For left, right, and top partial ternaries only -- return the
+        number of the vertex the diagram is centered on. As usual,
+        start with 0 = lower left, and successive numbers proceed
+        clockwise. */
+    public int getTriangleVertexNo() {
+        return mTriangleVertexNo;
     }
 }
