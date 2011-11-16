475dee94a15cee8eab9b0cad9c6d733ecd8defd8
emb <>
2011-11-16 17:38:07 +0000

    Reassigned all real work to Editor.java

--- nist/pededitor/EditPane.java
+++ nist/pededitor/EditPane.java
@@ -7,7 +7,6 @@ import java.util.*;
 
 public class EditPane extends ImagePane {
 
-    protected ArrayList<Point> vertices = new ArrayList<Point>();
     protected String filename = null;
     protected EditFrame parentFrame;
     protected Path2D spline;
@@ -30,13 +29,11 @@ public class EditPane extends ImagePane {
         return diagramOutline;
     }
 
-    class EditMouseAdapter extends MouseAdapter {
-        public void mousePressed(MouseEvent e) {
-            vertices.add(new Point(e.getX(), e.getY()));
-            recomputeSpline();
-            EditPane.this.repaint();
-        }
+    public EditFrame getParentFrame() {
+        return parentFrame;
+    }
 
+    class EditMouseAdapter extends MouseAdapter {
         public void mouseMoved(MouseEvent e) {
             repaintMaybe();
         }
@@ -50,60 +47,19 @@ public class EditPane extends ImagePane {
         }
 
         final void repaintMaybe() {
-            if (vertices.size() >= 1) {
-                EditPane.this.repaint();
-            }
+            EditPane.this.repaint();
         }
-    }
-
-    protected void recomputeSpline() {
-        int cnt = vertices.size();
-        if (cnt > 1) {
-            spline = (new CubicSpline2D(vertices.toArray(new Point[0]))).path();
-        }
-    }
-
-    /** @return the edit points that have been selected */
-    protected ArrayList<Point> getVertices() {
-        return vertices;
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (diagramOutline != null) {
-            ((Graphics2D) g).draw(diagramOutline);
-        }
 
-        int cnt = vertices.size();
-
-        if (cnt == 0) {
-            return;
-        }
-        
         Point mpos = null;
         try {
             mpos = getMousePosition();
         } catch (HeadlessException e) {
             mpos = null;
         }
-
-        Graphics2D g2d = (Graphics2D) g;
-            spline = (new CubicSpline2D(vertices.toArray(new Point[0]))).path();
-        
-        if (mpos != null) {
-            try {
-                vertices.add(new Point(mpos.x, mpos.y));
-                g.setColor(Color.RED);
-                g2d.draw((new CubicSpline2D
-                          (vertices.toArray(new Point[0]))).path());
-            } finally {
-                vertices.remove(cnt);
-            }
-        }
-
-        if (cnt >= 2) {
-            g.setColor(Color.GREEN);
-            g2d.draw(spline);
-        }
+        getParentFrame().getParentEditor().paintEditPane(g, mpos);
     }
 }
