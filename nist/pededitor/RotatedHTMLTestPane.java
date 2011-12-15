cba739549345ed259485cb4898cd4003ab739d30
emb <>
2011-12-15 17:57:00 +0000

    ...

--- nist/pededitor/RotatedHTMLTestPane.java
+++ nist/pededitor/RotatedHTMLTestPane.java
@@ -15,19 +15,19 @@ import javax.swing.plaf.basic.BasicHTML;
 public class RotatedHTMLTestPane extends JPanel {
     @Override
         public void paintComponent(Graphics g) {
-        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                             RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_RENDERING,
-                            RenderingHints.VALUE_RENDER_QUALITY);
 
-        String str = "<html><p><font color=\"red\">H<sub>2</sub>SO<sub>4</sub></font> "
-            + "100&deg;C"
+        String str = "<html><p>H<sub>2</sub>SO<sub>4</sub> "
+            + "<font color=\"red\">100&deg;C</font>"
             + "(&alpha; - &beta; &prime; <font size=\"+1\">&Delta;</font>)</font></p>"
-            + "<p>The lazy dog got run over.</p></html>";
-        g2.setColor(Color.red);
-        View view =  BasicHTML.createHTMLView(this, str);
+            + "<p>The lazy dog <u>got run over</u>.</p></html>";
+        
+        JLabel bogus = new JLabel(str);
+        bogus.setFont(new Font(null, 0, 32));
+        bogus.setForeground(Color.BLUE);
+        View view =  (View) bogus.getClientProperty("html");
 
         AffineTransform oldxform = g2.getTransform();
 
@@ -38,7 +38,7 @@ public class RotatedHTMLTestPane extends JPanel {
         for (double theta = 0.0; theta < Math.PI * 2 - 1e-6; theta += Math.PI / 4) {
             g2.translate(cx, cy);
             g2.rotate(theta, 0, 0);
-            view.paint(g, new Rectangle(10, 10, 300, 300));
+            view.paint(g, new Rectangle(10, 10, 500, 500));
             g2.setTransform(oldxform);
         }
 
