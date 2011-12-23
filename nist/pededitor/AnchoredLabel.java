efc45f2baeae86f48c0af08c0a13e1ace26543f4
emb <>
2011-12-23 21:00:37 +0000

    Moved font size and angle informatio into AnchoredLabel

--- nist/pededitor/AnchoredLabel.java
+++ nist/pededitor/AnchoredLabel.java
@@ -8,6 +8,10 @@ public class AnchoredLabel {
     double xWeight;
     double yWeight;
     String text;
+    double angle = 0.0;
+
+    /** A multiple of a standard font size (so 1.0 = normal). */
+    double fontSize;
 
     public AnchoredLabel
         (@JsonProperty("string") String text,
@@ -20,15 +24,22 @@ public class AnchoredLabel {
 
     public void setX(double x) { this.x = x; }
     public void setY(double y) { this.y = y; }
+    public void setXWeight(double xWeight) { this.xWeight = xWeight; }
+    public void setYWeight(double yWeight) { this.yWeight = yWeight; }
+    public void setAngle(double angle) { this.angle = angle; }
+    public void setFontSize(double fontSize) { this.fontSize = fontSize; }
+    public void setText(String text) { this.text = text; }
 
     public double getX() { return x; }
     public double getY() { return y; }
-    public String getString() { return text; }
+    public String getText() { return text; }
+    public double getAngle() { return angle; }
+    public double getFontSize() { return fontSize; }
     @JsonProperty("xWeight") public double getXWeight() { return xWeight; }
     @JsonProperty("yWeight") public double getYWeight() { return yWeight; }
 
     public String toString() {
         return "'" + text + "' x: " + x  + " y: " + y + " wx: " + xWeight
-            + " wy: " + yWeight;
+            + " wy: " + yWeight + " angle: " + angle + " fontSize: " + fontSize;
     }
 }
