4ac95e51c51b46706044faa00cf5391986761f71
emb <>
2011-12-29 18:43:50 +0000

    ...

---
+++ nist/pededitor/Arrow.java
@@ -0,0 +1,54 @@
+package gov.nist.pededitor;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+@JsonIgnoreProperties
+    ({"bounds2D", "bounds", "windingRule", "currentPoint"})
+public class Arrow extends Path2D.Double {
+    @JsonProperty double x;
+    @JsonProperty double y;
+    @JsonProperty double size;
+    @JsonProperty double theta;
+
+    /** @param size The "size" of the arrow is a bit arbitrary, but
+        it should be roughly equal to the line width of any line that
+        is attached to the arrow.
+    */
+    public Arrow(@JsonProperty("x") double x,
+                 @JsonProperty("y") double y,
+                 @JsonProperty("size") double size,
+                 @JsonProperty("theta") double theta) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.theta = theta;
+        addArrow(this, x, y, size, theta);
+    }
+
+    static public void addArrow
+        (Path2D.Double output, double x, double y,
+         double size, double theta) {
+        AffineTransform xform = AffineTransform.getTranslateInstance(x,y);
+        xform.rotate(theta);
+        xform.scale(size, size);
+        double[] points =
+            { 0.0, 0.0,
+              -3.2, -4.0,
+              8.0, 0.0,
+              -3.2, 4.0 };
+        xform.transform(points, 0, points, 0, points.length / 2);
+        output.moveTo(points[0], points[1]);
+        for (int i = 1; 2*i < points.length; ++i) {
+            output.lineTo(points[i*2], points[i*2+1]);
+        }
+        output.closePath();
+    }
+}
---
+++ nist/pededitor/VertexInfoDialog.java
@@ -0,0 +1,131 @@
+package gov.nist.pededitor;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/** GUI to display information about a tangency point in the
+    diagram. */
+public class VertexInfoDialog extends JDialog {
+
+    // TODO Use diagram components to determine slope.
+
+
+    public JLabel angle = new JLabel("180.00");
+    public JLabel slope = new JLabel("9999.99");
+    JLabel slopeLabel;
+    public JLabel lineWidth = new JLabel("0.00000");
+
+    protected void add(JComponent c, GridBagLayout gb,
+                       GridBagConstraints gbc) {
+        gb.setConstraints(c, gbc);
+        getContentPane().add(c);
+    }
+
+    public VertexInfoDialog(Frame owner) {
+        super(owner, "Tangent", false);
+
+        GridBagLayout gb = new GridBagLayout();
+        getContentPane().setLayout(gb);
+
+        Insets insets = new Insets(0, 3, 0, 3);
+        GridBagConstraints east = new GridBagConstraints();
+        east.anchor = GridBagConstraints.EAST;
+        east.insets = insets;
+        GridBagConstraints west = new GridBagConstraints();
+        west.anchor = GridBagConstraints.WEST;
+        west.insets = insets;
+
+        GridBagConstraints endRow = new GridBagConstraints();
+        endRow.anchor = GridBagConstraints.WEST;
+        endRow.gridwidth = GridBagConstraints.REMAINDER;
+
+        add(new JLabel("Angle:"), gb, east);
+        add(angle, gb, west);
+        add(new JLabel("\u00B0"), gb, endRow);
+
+        add(new JLabel("Slope:"), gb, east);
+        add(slope, gb, endRow);
+
+        add(new JLabel("Line width:"), gb, east);
+        add(lineWidth, gb, endRow);
+        pack();
+        setGradient(null);
+        setLineWidth(0.0);
+    }
+
+    public void setGradient(Point2D point) {
+        double x = (point == null) ? 0 : point.getX();
+        double y = (point == null) ? 0 : point.getY();
+        if (x == 0 && y == 0) {
+            angle.setText("");
+            slope.setText("");
+            return;
+        }
+
+        if (x == 0) {
+            slope.setText("");
+        } else {
+            setSlope(y/x);
+        }
+
+        setAngle(Math.atan2(y, x));
+    }
+
+    public void setAngle(double theta) {
+        double deg = (-theta / Math.PI) * 180;
+        if (deg < -90) {
+            deg += 180;
+        } else if (deg > 90) {
+            deg -= 180;
+        }
+
+        angle.setText(String.format("%.2f", deg));
+        repaint();
+    }
+
+    @JsonIgnore
+    public double getAngleDegrees() {
+        try {
+            return Double.valueOf(angle.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public double getAngle() {
+        return Compass.degreesToTheta(getAngleDegrees());
+    }
+
+    public void setSlope(double m) {
+        slope.setText(String.format("%.4f", m));
+        repaint();
+    }
+
+    public void setSlopeLabel(String label) {
+        slopeLabel.setText(label + ":");
+        repaint();
+    }
+
+    // TODO Kind of lame -- use full precision slope instead of using
+    // the displayed approximation.
+    public double getSlope() {
+        try {
+            return Double.valueOf(slope.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public void setLineWidth(double w) {
+        lineWidth.setText(String.format("%.5f", w));
+        repaint();
+    }
+    
+}
