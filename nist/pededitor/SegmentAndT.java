5de0d5daaa92d69e6d890a752cc47a75385687a1
emb <>
2012-01-31 22:15:43 +0000

    Pulled out of CubicSpline1D.java

---
+++ nist/pededitor/SegmentAndT.java
@@ -0,0 +1,15 @@
+package gov.nist.pededitor;
+
+public class SegmentAndT {
+    SegmentAndT(int segment, double t) {
+        this.segment = segment;
+        this.t = t;
+    }
+
+    public String toString() {
+        return "SegmentAndT[" + segment + ", " + t + "]";
+    }
+
+    int segment;
+    double t;
+}
