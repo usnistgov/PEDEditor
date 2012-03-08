522154cec00cc6fa9860af6dc703ef6c7b80519c
emb <>
2012-03-08 14:27:02 +0000

    Place arrow point, instead of back of arrowhead, at the specified location

--- nist/pededitor/Arrow.java
+++ nist/pededitor/Arrow.java
@@ -60,15 +60,56 @@ public class Arrow extends Path2D.Double {
         AffineTransform xform = AffineTransform.getTranslateInstance(x,y);
         xform.rotate(theta);
         xform.scale(size, size);
+
+        // Sharp, pointy arrows ordinarily look best, but those can be
+        // difficult to coordinate with the segments that form the
+        // arrow shafts.
+
+        // Line segments end with r = 0.5 * linewidth semicircles, and
+        // if an arrowpoint ends precisely where the line segment
+        // does, the bulbous tip of the line sticks out past the
+        // arrowpoint in a really ugly way. Somehow deciding to end
+        // the line segment early if there is an arrow attached,
+        // however, becomes a logical mess. Extending the arrow far
+        // past the bulb to keep it sharp implies that the true end of
+        // the arrow is well past the position its (x,y) value
+        // implies. Solution: extend the arrow just enough to hide the
+        // semicircle, which requires blunting the tip slightly.
+
+        // The arrow extends from the coordinate (-7.0 times the line
+        // width in the arrow's pointing dimension, 3.2 times the line
+        // width in the perpendicular dimension) to (0.1779, 0.47),
+        // the point of tangency from (-7.0, 3.2) to the circle of
+        // radius 0.5 centered at the origin. From there, the arrow
+        // smoothly turns around a quadratic Bezier to the blunted tip
+        // at (0.5, 0), and then symmetrically reverses course to draw
+        // the other half of the arrow.
+
         double[] points =
-            { 0.0, 0.0,
-              -2.56, -3.2,
-              6.4, 0.0,
-              -2.56, 3.2 };
+            { -4.5, 0.0, // Back of arrow's spine
+              -7.0, 3.2, // Top left barb
+              0.1778999, 0.467281, // Tangency with r=0.5 circle at (0,0)
+              0.5, 0.3446534, // Control point -- intersection of
+                              // previous segment and line { x = 0.5 }
+              0.5, 0, // Blunted tip of arrow
+              0.5, -0.3446534, // And then the arrow's other side
+              0.1778999, -0.467281,
+              -7.0, -3.2 };
         xform.transform(points, 0, points, 0, points.length / 2);
-        output.moveTo(points[0], points[1]);
-        for (int i = 1; 2*i < points.length; ++i) {
-            output.lineTo(points[i*2], points[i*2+1]);
+
+        int p = 0;
+        output.moveTo(points[p], points[p+1]); p += 2;
+        output.lineTo(points[p], points[p+1]); p += 2;
+        output.lineTo(points[p], points[p+1]); p += 2;
+        output.quadTo(points[p], points[p+1],
+                      points[p+2], points[p+3]);
+        p += 4;
+        output.quadTo(points[p], points[p+1],
+                      points[p+2], points[p+3]);
+        p += 4;
+        output.lineTo(points[p], points[p+1]); p += 2;
+        if (p != points.length) {
+            throw new IllegalStateException();
         }
         output.closePath();
     }
