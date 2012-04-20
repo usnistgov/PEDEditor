b2b9ea62487272244bb4554d34590957f9b3ad78
emb <>
2012-04-20 21:55:02 +0000

    Renamed from Selectable to Decoration; split off DecorationHandle

--- nist/pededitor/Decoration.java
+++ nist/pededitor/Decoration.java
@@ -1,26 +1,23 @@
 package gov.nist.pededitor;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.*;
 
 /** Interface that permits common operations to be applied to
     different kinds of graphics objects */
-public interface Selectable {
-    /** @return the new selection that should result from the removal
+public interface Decoration {
+    void draw(Graphics2D g, double scale);
+    /** @return the new SelectionHandle that should result from the removal
         of this object, or null if the removal of this object should
         leave the current selection empty. */
-    Selectable remove();
-    void move(Point2D dest);
-
-    /** Copy this selection, placing the copy at dest. Return the
-        Selectable object that represents the copy. */
-    Selectable copy(Point2D dest);
-    Point2D.Double getLocation();
-    boolean isEditable();
-    void edit();
+    DecorationHandle remove();
     void setLineWidth(double lineWidth);
     double getLineWidth();
     void setLineStyle(StandardStroke lineStyle);
     Color getColor();
     void setColor(Color color);
+    Rectangle2D getBounds();
+    Rectangle2D getBounds(AffineTransform xform);
+    DecorationHandle[] getHandles();
 }
