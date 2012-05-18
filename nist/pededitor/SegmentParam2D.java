f011961b3cd226ad8d4ee4fc5bf4e6316e7a009b
emb <>
2012-05-18 12:26:41 +0000

    Line segment specialization of Parameterization2D

---
+++ nist/pededitor/SegmentParam2D.java
@@ -0,0 +1,93 @@
+package gov.nist.pededitor;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/** Parameterize a line segment. */
+public class SegmentParam2D
+    extends Parameterization2DAdapter {
+
+    /** @param p0 The position at time t=0, even if t0 != 0.
+
+        @param pEnd The position at time t=1, even if t1 != 1.
+
+        @param t0 The minimum t value (normally 0).
+
+        @param t1 The minimum t value (normally 1).
+    */
+    public SegmentParam2D
+        (Point2D p0, Point2D pEnd, double t0, double t1) {
+        super(p0, pEnd, t0, t1);
+    }
+
+    @Override public SegmentParam2D clone() {
+        return new SegmentParam2D(p0, pEnd, t0, t1);
+    }
+        
+    @Override public SegmentParam2D derivative() {
+        Point2D.Double g = getGradient(0);
+        return new SegmentParam2D(g, g, t0, t1);
+    }
+
+    @Override public CurveDistance distance(Point2D p) {
+        CurveDistance output =
+            CurveDistance.pointSegmentDistance(p, getStart(), getEnd());
+        if (t1 > t0) {
+            output.t = t0 + output.t / (t1 - t0);
+        }
+        return output;
+    }
+
+    @Override public CurveDistance distance
+        (Point2D p, double maxError, double maxIterations) {
+        return distance(p);
+    }
+
+    @Override public Rectangle2D.Double getBounds() {
+        Rectangle2D r = (new Line2D.Double(getLocation(t0), getLocation(t1)))
+            .getBounds2D();
+        return new Rectangle2D.Double(r.getX(), r.getY(), 
+                                      r.getWidth(), r.getHeight());
+    }
+        
+    @Override public Point2D.Double getGradient(double t) {
+        return new Point2D.Double(pEnd.x - p0.x, pEnd.y - p0.y);
+    }
+
+    @Override public Point2D.Double getLocation(double t) {
+        return new Point2D.Double
+            (p0.x + (pEnd.x - p0.x) * t, p0.y + (pEnd.y - p0.y) * t);
+    }
+
+    @Override public double[] segIntersections(Line2D segment) {
+        double t = Duh.segmentIntersectionT
+            (p0, pEnd, segment.getP1(), segment.getP2());
+        if (inRange(t)) {
+            return new double[] { t };
+        } else {
+            return new double[0];
+        }
+    }
+
+    @Override public double[] lineIntersections(Line2D segment) {
+        double t = Duh.lineIntersectionT
+            (p0, pEnd, segment.getP1(), segment.getP2());
+        if (inRange(t)) {
+            return new double[] { t };
+        } else {
+            return new double[0];
+        }
+    }
+
+    @Override public String toString() {
+        StringBuilder s = new StringBuilder
+            (getClass().getSimpleName() + "[" + Duh.toString(getStart()) + ", "
+             + Duh.toString(getEnd()));
+        if (getMinT() != 0 || getMaxT() != 1) {
+            s.append(" t in [" + getMinT() + ", " + getMaxT() + "]");
+        }
+        s.append("]");
+        return s.toString();
+    }
+}
