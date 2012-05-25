f1740d46419c5f95b3498bf31f87422a37894e50
emb <>
2012-05-25 13:04:35 +0000

    Allow decimal subscripts

--- nist/pededitor/ChemicalString.java
+++ nist/pededitor/ChemicalString.java
@@ -13,13 +13,14 @@ public class ChemicalString {
     final static String element =
         "H|He|Li|Be|B|C|N|O|F|Ne|Na|Mg|Al|Si|P|S|Cl|Ar|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|U|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Uun|Uuu|Uub|Uuq|Uuh";
     final static String ion = "[\u207a\u207b]"; // superscript+ or superscript-
+    final static String subscript = "\\d+(?:\\.\\d+)?";
     final static String subscriptNeeded = "((?:(?:" + element + ")|\\))" + ion + "*)"
-        + "(\\d++)";
+        + "(" + subscript + ")";
     final static String subscriptNeededReplacement = "$1<sub>$2</sub>";
     static Pattern subscriptNeededPattern = null;
 
     final static String elementWithCount = "(" + element + ")" + ion + "*"
-        + "(<sub>\\d+</sub>)?" + ion + "*";
+        + "(<sub>" + subscript + "</sub>)?" + ion + "*";
     final static String simpleCompound = "(?<![A-Za-z]>("+ elementWithCount + ")+";
 
     // An extended compound, with parenthesized subunits, cannot be
