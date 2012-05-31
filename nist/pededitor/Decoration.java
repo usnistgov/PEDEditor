aabdb026f30455f33e998636349768c2a0e827fc
emb <>
2012-05-31 13:24:24 +0000

    Add getMovementHandles()

--- nist/pededitor/Decoration.java
+++ nist/pededitor/Decoration.java
@@ -17,4 +17,14 @@ public interface Decoration {
     Color getColor();
     void setColor(Color color);
     DecorationHandle[] getHandles();
+
+    /* getMovementHandles() returns a subset of the handles of this
+     decoration that is sufficient to permit it to move. If this
+     decoration is defined in terms of positions on another
+     decoration, then the returned list should be empty, because
+     moving that other decoration will move this with it. If this
+     decoration has a fixed orientation but many potential grabbing
+     points, then the list should contain just one handle, since
+     moving that one will move the others along with it. */
+    DecorationHandle[] getMovementHandles();
 }
