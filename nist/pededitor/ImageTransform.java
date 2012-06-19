9a4b8a47e997c68031aafc66ed76a93569f34990
emb <>
2012-06-19 22:13:09 +0000

    First attempt at multithreading, foiled by apparently synchronous BufferedImage

--- nist/pededitor/ImageTransform.java
+++ nist/pededitor/ImageTransform.java
@@ -4,6 +4,9 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
 import java.io.*;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
 import javax.imageio.ImageIO;
 
 /** This module provides a method to transform an input image to an
@@ -11,6 +14,7 @@ import javax.imageio.ImageIO;
     output should support the #call(x,y) method), #max_output_x, and
     #max_output_y. */
 public class ImageTransform {
+    static final ForkJoinPool mainPool = new ForkJoinPool();
 
     /** run() with default size and black background. */
     public static BufferedImage run(PolygonTransform xform,
@@ -27,6 +31,172 @@ public class ImageTransform {
         int width = (int) Math.ceil(b.x + b.width);
         int height = (int) Math.ceil(b.y + b.height);
         return run(xform, imageIn, background, new Dimension(width, height));
+    }
+
+    static class RecursiveTransformer extends RecursiveAction {
+        /**
+		 * 
+		 */
+        private static final long serialVersionUID = -2489117155798819683L;
+
+        final public BufferedImage input;
+        final public Rectangle outputBounds;
+        final public BufferedImage output;
+        final public Color background;
+        final int sampleCnt;
+        /** Inverse transformation, to measure the color of the input
+            image at each pixel of the output image. */
+        final Transform2D inverseTransform;
+
+        RecursiveTransformer(BufferedImage input,
+                             BufferedImage output, Rectangle outputBounds,
+                             Transform2D inverseTransform, Color background,
+                             int sampleCnt) {
+            this.input = input;
+            this.inverseTransform = inverseTransform;
+            this.output = output;
+            this.outputBounds = outputBounds;
+            this.background = background;
+            this.sampleCnt = sampleCnt;
+        }
+
+        protected void compute() {
+            if (outputBounds.width >= 2
+            		&& outputBounds.width * outputBounds.height > 300 * 300) {
+                RecursiveTransformer leftHalf
+                    = new RecursiveTransformer
+                    (input, output,
+                     new Rectangle(outputBounds.x, outputBounds.y,
+                                   outputBounds.width/2, outputBounds.height),
+                     inverseTransform, background, sampleCnt);
+                RecursiveTransformer rightHalf
+                    = new RecursiveTransformer
+                    (input, output,
+                     new Rectangle(outputBounds.x + outputBounds.width/2,
+                                   outputBounds.y,
+                                   (outputBounds.width + 1)/2,
+                                   outputBounds.height),
+                     inverseTransform, background, sampleCnt);
+                invokeAll(leftHalf, rightHalf);
+            } else {
+                run(input, output, outputBounds, inverseTransform,
+                    background, sampleCnt);
+            }
+        }
+    }
+
+    static double[] randCache = null;
+
+    static void initializeRandCache() {
+        // random() turns out to slow things down considerably, so reuse
+        // old random values -- we don't need perfection here.
+
+        if (randCache == null) {
+            randCache = new double[0x1000];
+            for (int i = 0; i < randCache.length; ++i) {
+                randCache[i] = Math.random();
+            }
+        }
+    }
+
+
+    static boolean requiresThreading(Rectangle rect) {
+        return rect.width * rect.height > 100 * 100;
+    }
+
+    /** @param sampleCnt number of samples to take per pixel */
+    static void run(BufferedImage input,
+             BufferedImage output, Rectangle outputBounds,
+             Transform2D inverseTransform, Color background,
+             int sampleCnt) {
+        int samplesPerPixel = sampleCnt * sampleCnt;
+        double inWidth = input.getWidth();
+        double inHeight = input.getHeight();
+
+        // Transform a pixel's worth of points at once for better
+        // speed. (This wouldn't be necessary in C++, which has stack
+        // objects with zero allocation overhead. The color
+        // manipulation, too, could be done much nicer in C++ as
+        // zero-overhead four-byte objects.)
+        double points[] = new double[samplesPerPixel * 2];
+        int backRGB = background.getRGB();
+        
+
+        if (randCache == null) {
+            randCache = new double[0x1000];
+            for (int i = 0; i < randCache.length; ++i) {
+                randCache[i] = Math.random();
+            }
+        }
+
+        initializeRandCache();
+        int randCachePos = 0;
+
+        int xMax = outputBounds.x + outputBounds.width;
+        int yMax = outputBounds.y + outputBounds.height;
+
+        for (int x = outputBounds.x; x < xMax; ++x) {
+            for (int y = outputBounds.y; y < yMax; ++y) {
+                // The image is over-sampled (with respect to the size
+                // of an output pixel) to reduce aliasing: output
+                // pixels are divided into sampleCnt x sampleCnt
+                // sub-pixels, and the colors of those sub-pixels are
+                // averaged to obtain the color of the output pixel.
+                // (The value of sampleCnt setting is pretty
+                // arbitrary: up to 11 times smaller than an output
+                // pixel if the output image is much smaller than the
+                // input one, and as little as 3 times smaller than
+                // the output pixel if the output image is as big or
+                // bigger than the input image.)
+
+                // Each sub-pixel is sampled not in its center, but in
+                // a uniformly random location; this randomization
+                // should reduce systematic aliasing artifacts.
+                {
+                    int pos = 0;
+                    for (int xsub = 0; xsub < sampleCnt; ++xsub) {
+                        for (int ysub = 0; ysub < sampleCnt; ++ysub) {
+                            points[pos++] = x +
+                                (xsub + randCache[(++randCachePos) & 0xfff]) / sampleCnt;
+                            points[pos++] = y +
+                                (ysub + randCache[(++randCachePos) & 0xfff]) / sampleCnt;
+                        }
+                    }
+                }
+
+                try {
+                    inverseTransform.transform(points, 0, points, 0,
+                                               samplesPerPixel);
+                    // Now points[] contains the (x,y) coordinates of
+                    // points in the input image whose colors should
+                    // be averaged to set the color for this pixel in
+                    // the output image.
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+                    for (int pos = 0; pos < points.length; pos += 2) {
+                        double xd = points[pos];
+                        double yd = points[pos+1];
+                        int rgb = (xd >= 0. && xd < inWidth && yd >= 0. && yd < inHeight)
+                            ? input.getRGB((int) xd, (int) yd) : backRGB;
+                        r += (rgb >> 16) & 255;
+                        g += (rgb >> 8) & 255;
+                        b += rgb & 255;
+                    }
+
+                    // Round values over 1/2 up in the integer
+                    // division
+                    int half = samplesPerPixel / 2;
+                    r = (r + half) / samplesPerPixel;
+                    g = (g + half) / samplesPerPixel;
+                    b = (b + half) / samplesPerPixel;
+
+                    output.setRGB(x, y, (r << 16) + (g << 8) + b);
+                } catch (UnsolvableException e) {
+                    output.setRGB(x, y, backRGB);
+                }
+            }
+        }
     }
 
     /**  The scale of the output is 1 pixel = 1 unit. The minimum x and
@@ -60,94 +230,10 @@ public class ImageTransform {
         double ipixels = (ib.width+1) * (ib.height+1);
         int sampleCnt = (int) Math.round
             (Math.max(2, Math.min(11, 2 * Math.sqrt(ipixels / (width * height)))));
-        int samplesPerPixel = sampleCnt * sampleCnt;
-        double inWidth = input.getWidth();
-        double inHeight = input.getHeight();
-        int totalSampleCnt = 0;
-
-        // Transform a pixel's worth of points at once for better
-        // speed. (This wouldn't be necessary in C++, which has stack
-        // objects with zero allocation overhead. The color
-        // manipulation, too, could be done much nicer in C++ as
-        // zero-overhead four-byte objects.)
-        double points[] = new double[samplesPerPixel * 2];
-        int backRGB = background.getRGB();
-        
-
-        // random() turns out to slow things down considerably, so reuse
-        // old random values -- we don't need perfection here.
-        double randCache[] = new double[0x1000];
-        for (int i = 0; i < randCache.length; ++i) {
-            randCache[i] = Math.random();
-        }
-        int randCachePos = 0;
-
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // The image is over-sampled (with respect to the size
-                // of an output pixel) to reduce aliasing: output
-                // pixels are divided into sampleCnt x sampleCnt
-                // sub-pixels, and the colors of those sub-pixels are
-                // averaged to obtain the color of the output pixel.
-                // (The value of sampleCnt setting is pretty
-                // arbitrary: up to 11 times smaller than an output
-                // pixel if the output image is much smaller than the
-                // input one, and as little as 3 times smaller than
-                // the output pixel if the output image is as big or
-                // bigger than the input image.)
-
-                // Each sub-pixel is sampled not in its center, but in
-                // a uniformly random location; this randomization
-                // should reduce systematic aliasing artifacts.
-                {
-                    int pos = 0;
-                    for (int xsub = 0; xsub < sampleCnt; ++xsub) {
-                        for (int ysub = 0; ysub < sampleCnt; ++ysub) {
-                            points[pos++] = x +
-                                (xsub + randCache[(++randCachePos) & 0xfff]) / sampleCnt;
-                            points[pos++] = y +
-                                (ysub + randCache[(++randCachePos) & 0xfff]) / sampleCnt;
-                        }
-                    }
-                }
-
-                try {
-                    xformi.transform(points, 0, points, 0, samplesPerPixel);
-                    // Now points[] contains the (x,y) coordinates of
-                    // points in the input image whose colors should
-                    // be averaged to set the color for this pixel in
-                    // the output image.
-                    totalSampleCnt += samplesPerPixel;
-                    int r = 0;
-                    int g = 0;
-                    int b = 0;
-                    for (int pos = 0; pos < points.length; pos += 2) {
-                        double xd = points[pos];
-                        double yd = points[pos+1];
-                        int rgb = (xd >= 0. && xd < inWidth && yd >= 0. && yd < inHeight)
-                            ? input.getRGB((int) xd, (int) yd) : backRGB;
-                        r += (rgb >> 16) & 255;
-                        g += (rgb >> 8) & 255;
-                        b += rgb & 255;
-                    }
-
-                    // Round values over 1/2 up in the integer
-                    // division
-                    int half = samplesPerPixel / 2;
-                    r = (r + half) / samplesPerPixel;
-                    g = (g + half) / samplesPerPixel;
-                    b = (b + half) / samplesPerPixel;
-
-                    output.setRGB(x, y, (r << 16) + (g << 8) + b);
-                } catch (UnsolvableException e) {
-                    output.setRGB(x, y, backRGB);
-                }
-            }
-        }
-
-        System.err.println(totalSampleCnt + " samples used.");
+        Rectangle outputBounds = new Rectangle(0, 0, size.width, size.height);
+        mainPool.invoke(new RecursiveTransformer(input, output, outputBounds,
+                                                 xformi, background, sampleCnt));
         s.ping();
-
         return output;
     }
 
@@ -240,8 +326,11 @@ public class ImageTransform {
             xform.concatenate(af);
         }
 
-        int outWidth = 800;
-        int outHeight = 600;
+        // int outWidth = 800;
+        // int outHeight = 600;
+
+        int outWidth = 4800;
+        int outHeight = 3600;
 
         System.out.println(xform);
 
