2b2837f5937f453e52cad13debcb862a317075e9
emb <>
2012-08-03 15:28:42 +0000

    BoundedParam2D->Param2D

--- nist/pededitor/SegmentParam2D.java
+++ nist/pededitor/SegmentParam2D.java
@@ -5,34 +5,29 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 /** Parameterize a line segment. */
-public class SegmentParam2D
-    extends Parameterization2DAdapter {
+public class SegmentParam2D extends Param2DAdapter
+    implements Param2D {
+    Point2D.Double p0;
+    Point2D.Double pEnd;
 
-    /** @param p0 The position at time t=0, even if t0 != 0.
-
-        @param pEnd The position at time t=1, even if t1 != 1.
-
-        @param t0 The minimum t value (normally 0).
-
-        @param t1 The minimum t value (normally 1).
+    /** @param p0 The position at time t=0.
+        @param pEnd The position at time t=1
     */
     public SegmentParam2D
-        (Point2D p0, Point2D pEnd, double t0, double t1) {
-        super(p0, pEnd, t0, t1);
-    }
-
-    @Override public SegmentParam2D clone() {
-        return new SegmentParam2D(p0, pEnd, t0, t1);
+        (Point2D p0, Point2D pEnd) {
+        this.p0 = new Point2D.Double(p0.getX(), p0.getY());
+        this.pEnd = new Point2D.Double(pEnd.getX(), pEnd.getY());
     }
         
-    @Override public SegmentParam2D derivative() {
+    @Override protected SegmentParam2D computeDerivative() {
         Point2D.Double g = getDerivative(0);
-        return new SegmentParam2D(g, g, t0, t1);
+        return new SegmentParam2D(g, g);
     }
 
-    @Override public CurveDistanceRange distance(Point2D p) {
-        CurveDistance res =
-            CurveDistance.pointSegmentDistance(p, getStart(), getEnd());
+    @Override public CurveDistanceRange distance
+        (Point2D p, double t0, double t1) {
+        CurveDistance res = CurveDistance.pointSegmentDistance
+            (p, getLocation(t0), getLocation(t1));
         if (t1 > t0) {
             res.t = t0 + res.t / (t1 - t0);
         } else {
@@ -42,21 +37,24 @@ public class SegmentParam2D
     }
 
     @Override public CurveDistanceRange distance
-        (Point2D p, double maxError, int maxSteps) {
-        return distance(p);
+        (Point2D p, double maxError, int maxSteps, double t0, double t1) {
+        return distance(p, t0, t1);
     }
 
-    @Override public Rectangle2D.Double getBounds() {
+    @Override public Rectangle2D.Double getBounds(double t0, double t1) {
         Rectangle2D r = (new Line2D.Double(getLocation(t0), getLocation(t1)))
             .getBounds2D();
         return new Rectangle2D.Double(r.getX(), r.getY(), 
                                       r.getWidth(), r.getHeight());
     }
 
-    @Override public double[] getBounds(double xc, double yc) {
-        double d1 = p0.x * xc + p0.y * yc;
-        double d2 = pEnd.x * xc + pEnd.y * yc;
-        return new double[] { Math.min(d1,d2), Math.max(d1,d2) };
+    @Override public double[] getBounds
+        (double xc, double yc, double t0, double t1) {
+        Point2D.Double p0 = getLocation(t0);
+        Point2D.Double p1 = getLocation(t1);
+        double d0 = p0.x * xc + p0.y * yc;
+        double d1 = p1.x * xc + p1.y * yc;
+        return new double[] { Math.min(d0,d1), Math.max(d0,d1) };
     }
         
     @Override public Point2D.Double getDerivative(double t) {
@@ -68,20 +66,22 @@ public class SegmentParam2D
             (p0.x + (pEnd.x - p0.x) * t, p0.y + (pEnd.y - p0.y) * t);
     }
 
-    @Override public double[] segIntersections(Line2D segment) {
+    @Override public double[] segIntersections
+        (Line2D segment, double t0, double t1) {
         double t = Duh.segmentIntersectionT
             (p0, pEnd, segment.getP1(), segment.getP2());
-        if (inDomain(t)) {
+        if (t >= t0 && t <= t1) {
             return new double[] { t };
         } else {
             return new double[0];
         }
     }
 
-    @Override public double[] lineIntersections(Line2D segment) {
+    @Override public double[] lineIntersections
+        (Line2D segment, double t0, double t1) {
         double t = Duh.lineIntersectionT
             (p0, pEnd, segment.getP1(), segment.getP2());
-        if (inDomain(t)) {
+        if (t >= t0 && t <= t1) {
             return new double[] { t };
         } else {
             return new double[0];
@@ -90,17 +90,9 @@ public class SegmentParam2D
 
     @Override public String toString() {
         StringBuilder s = new StringBuilder
-            (getClass().getSimpleName() + "[" + Duh.toString(getStart()) + ", "
-             + Duh.toString(getEnd()));
-        if (getMinT() != 0 || getMaxT() != 1) {
-            s.append(" t in [" + getMinT() + ", " + getMaxT() + "]");
-        }
+            (getClass().getSimpleName() + "[" + Duh.toString(p0) + ", "
+             + Duh.toString(pEnd));
         s.append("]");
         return s.toString();
-    }
-
-    @Override public Parameterization2D createSubset(double minT,
-                                                     double maxT) {
-        return new SegmentParam2D(p0, pEnd, minT, maxT);
     }
 }
