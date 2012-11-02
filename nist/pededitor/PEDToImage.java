92aa3303b7ad0924171ffce523d8371b5a7556a3
emb <>
2012-11-02 18:22:49 +0000

    Allow PDF output type; infer output type from output file extension

--- nist/pededitor/PEDToImage.java
+++ nist/pededitor/PEDToImage.java
@@ -3,19 +3,75 @@ package gov.nist.pededitor;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
 /** Wrapper class for conversion of PED files to JPEG files. */
 public class PEDToImage {
-    public static void main(String format, String[] args) {
-        if (args.length != 4) {
-            System.err.println
-                ("Usage: java -jar PEDTo" + format + ".jar [PED filename] ["
-                 + format + " filename]\n"
-                 + "          [max image width] [max image height]\n\n"
-                 + "Convert the given PED file to " + format + " format.");
-            System.exit(1);
+
+    static void help() {
+        System.err.println
+            ("Usage:\n\n"
+             + "    java -jar PEDToImage.jar <PED file> <PDF file>\n\n"
+             + "         or\n\n"
+             + "    java -jar PEDToImage.jar <PED file> <image file> <width> <height>\n\n"
+             + "Supported image formats include GIF, JPEG, and PNG.");
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            help();
         }
         String ifn = args[0];
         String ofn = args[1];
+        String ext = Editor.getExtension(ofn);
+        File ofh = new File(ofn);
+
+        if (ext == null) {
+            throw new IllegalArgumentException
+                ("Output file '" + ofn + "' missing extension (such as .jpg, .gif, .png, or .pdf)");
+        }
+
+        boolean isPDF = ext.equalsIgnoreCase("pdf");
+
+        if (isPDF) {
+            if (args.length != 2) {
+                help();
+            }
+
+            Diagram d;
+            try {
+                d = Diagram.loadFrom(new File(ifn));
+            } catch (IOException x) {
+                throw new IllegalArgumentException
+                    ("Invalid input file '" + args[0] + "': " + x);
+            }
+            d.saveAsPDF(ofh);
+            return;
+        }
+
+        if (args.length != 4) {
+            help();
+        }
+
+        if (!isPDF) {
+            String[] imageExts = ImageIO.getReaderFileSuffixes();
+
+            boolean foundMatch = false;
+            for (String x: imageExts) {
+                if (ext.equalsIgnoreCase(x)) {
+                    ext = x;
+                    foundMatch = true;
+                    break;
+                }
+            }
+
+            if (!foundMatch) {
+                throw new IllegalArgumentException
+                    ("Unsupported image format '" + ext + "'");
+            }
+        }
+        
         int width;
         try {
             width = Integer.parseInt(args[2]);
@@ -37,8 +93,9 @@ public class PEDToImage {
             throw new IllegalArgumentException
                 ("Invalid input file '" + args[0] + "': " + x);
         }
+
         try {
-            d.saveAsImage(new File(ofn), format, width, height);
+            d.saveAsImage(ofh, ext, width, height);
         } catch (IOException x) {
             throw new IllegalArgumentException
                 ("Invalid output file '" + args[1] + "': " + x);
