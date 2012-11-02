377f961fba59f22a94bffb34586a4f939eae20c9
emb <>
2012-11-02 18:26:32 +0000

    Superceded by PEDToImage

--- nist/pededitor/PEDToJPEG.java
+++
@@ -1,8 +0,0 @@
-package gov.nist.pededitor;
-
-/** Wrapper class for conversion of PED files to JPEG files. */
-public class PEDToJPEG {
-    public static void main(String[] args) {
-        PEDToImage.main("JPEG", args);
-    }
-}
