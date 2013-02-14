8fff18c99e9c6fb4a41d79f13b6044fec28680af
Eric Boesch <ericboesch@gmail.com>
2013-02-14 15:43:11 +0000

    Bug fix: only convert spaces to tabs at start of line

--- nist/pededitor/Tabify.java
+++ nist/pededitor/Tabify.java
@@ -1,15 +1,14 @@
 package gov.nist.pededitor;
 
-/** GUI for selecting a label string and an anchoring position for
-    that label. */
 public class Tabify {
-    /* Convert every set of 8 consecutive spaces into tabs. */
+    /* Convert every set of 8 consecutive spaces that start a line into tabs. */
     static String tabify(String s) {
         StringBuilder res = new StringBuilder();
         int spaceCnt = 0;
+        boolean lineStart = true;
         for (int i = 0, len = s.length(); i < len; ++i) {
         	char ch = s.charAt(i);
-            if (ch == ' ') {
+            if (lineStart && ch == ' ') {
                 if (++spaceCnt == 8) {
                     res.append('\t');
                     spaceCnt = 0;
@@ -19,6 +18,7 @@ public class Tabify {
                     res.append(' ');
                 }
                 res.append(ch);
+                lineStart = (ch == '\n');
             }
         }
         return res.toString();
