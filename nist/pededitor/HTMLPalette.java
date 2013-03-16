acb33c25742c7a7543987ab4051789543611f6ee
Eric Boesch <ericboesch@gmail.com>
2013-03-16 06:14:23 +0100

    By consensus, bringing back the 'line break' option

--- nist/pededitor/HTMLPalette.java
+++ nist/pededitor/HTMLPalette.java
@@ -3,6 +3,7 @@ package gov.nist.pededitor;
 public class HTMLPalette extends StringPalette {
     public HTMLPalette() {
         super(new Object[][] {
+                {"<br>\n", "<html>line <br> break</html>"},
                 {"&lt;", "<"},
                 {"&gt;", ">"},
                 {"&amp;", "&"},
