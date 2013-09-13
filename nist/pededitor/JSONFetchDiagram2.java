7bc22d11ac1e38b9c2be6fa8d61e1f6286b7bf61
Eric Boesch <ericboesch@gmail.com>
2013-09-13 21:49:39 +0100

    Limit Viewer functionality. Use a SingleInstanceService that runs as a demon to reduce reload time.

--- nist/pededitor/JSONFetchDiagram2.java
+++ nist/pededitor/JSONFetchDiagram2.java
@@ -1,13 +1,19 @@
 package gov.nist.pededitor;
 
-import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+
+import javax.jnlp.ServiceManager;
+import javax.jnlp.SingleInstanceListener;
+import javax.jnlp.SingleInstanceService;
+import javax.jnlp.UnavailableServiceException;
 
 /** Class to start the PED Editor as a PED Viewer. The differences are
     that the editable flag is off by default, and the PED file is
@@ -15,11 +21,44 @@ import java.util.regex.PatternSyntaxException;
 
 public class JSONFetchDiagram2 {
 
+    /** Task that gets called regularly to see if any Editor objects
+        are open. A slightly better solution would be to check after a
+        given delay since the last non Editor closed, but that takes a
+        little more code. Instead, just check every 30 minutes whether
+        any Editors are open. */
+    static class ShutdownChecker extends TimerTask {
+        @Override public void run() {
+            int cnt = Editor.getOpenEditors().size();
+            if (cnt == 0) {
+                System.exit(0);
+            }
+        }
+    }
+
+    static class Listener implements SingleInstanceListener {
+        Timer closer = new Timer("ShutdownChecker", false);
+        TimerTask closeTask;
+        long delay = 60*1000*30; // 30 minutes in msecs
+
+        @Override public void newActivation(String[] args) {
+            run(args);
+            if (closeTask != null) {
+                closeTask.cancel();
+            }
+            closeTask = new ShutdownChecker();
+            closer.scheduleAtFixedRate(closeTask, delay, delay);
+        }
+    }
+
     public static void run(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException
+                ("run() via JWS takes exactly 1 argument");
+        }
         run(args[0]);
     }
 
-    public static void run(String urlStr) {
+    public static Editor run(String urlStr) {
         URL url = null;
 
         try {
@@ -36,17 +75,50 @@ public class JSONFetchDiagram2 {
             Diagram d = Diagram.loadFrom(connection.getInputStream());
             Editor e = new Editor();
             e.copyFrom(d);
-            e.initializeGUI();
-            e.editFrame.setReloadVisible(false);
+
+            // Cut out all the functions that the viewer doesn't need.
+
+            EditFrame ef = e.editFrame;
+            ef.setNewDiagramVisible(false);
+            ef.setOpenVisible(false);
+            ef.setReloadVisible(false);
+            ef.setEditable(false);
+            ef.editingEnabled.setVisible(false);
+            ef.mnTags.setVisible(false);
+            ef.mnKeys.setVisible(false);
+            ef.mnZoomIn.setVisible(false);
+            ef.mnZoomOut.setVisible(false);
+            ef.mnExportText.setVisible(false);
+            ef.mnCopyFormulas.setVisible(false);
+            ef.mnAutoPosition.setVisible(false);
+            ef.mnJumpToSelection.setVisible(false);
+            ef.mnNearestPoint.setVisible(false);
+            ef.mnNearestCurve.setVisible(false);
+            ef.mnUnstickMouse.setVisible(false);
+            ef.shortHelpFile = "viewhelp1.html";
+            e.mouseDragDistance = 0;
             e.detachOriginalImage();
+            try {
+                e.removeVariable("page X");
+            } catch (CannotDeletePrincipalVariableException e1) {
+                // OK, let it be
+            }
+            try {
+                e.removeVariable("page Y");
+            } catch (CannotDeletePrincipalVariableException e1) {
+                // OK, let it be
+            }
+            e.setSaveNeeded(false);
+            e.initializeGUI();
+            ef.setVertexInfoVisible(false);
             e.bestFit();
-            e.editFrame.setEditable(false);
+            return e;
         } catch (IOException x) {
             if (url != null) {
                 System.err.println("While connecting to " + url + ":");
             }
             System.err.println("Error " + x);
-            return;
+            return null;
         } catch (PatternSyntaxException x) {
             throw new IllegalStateException("Pattern could not compile: " + x);
         }
@@ -62,19 +134,15 @@ public class JSONFetchDiagram2 {
     }
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new ArgsRunnable(args) {
-                @Override public void run() {
-                    try {
-                        if (args.length >= 1) {
-                            JSONFetchDiagram2.run(args);
-                        } else {
-                            Editor app = new Editor();
-                            app.run(null);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+        try {
+            SingleInstanceService sis = (SingleInstanceService)
+                ServiceManager.lookup("javax.jnlp.SingleInstanceService");
+            Listener listen = new Listener();
+            sis.addSingleInstanceListener(listen);
+            listen.newActivation(args);
+        } catch(UnavailableServiceException x) {
+            // I guess we're not running via JWS...
+            JSONFetchDiagram2.run(args);
+        }
     }
 }
