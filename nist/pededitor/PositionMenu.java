d00758c9d3f42992c49f0ce83154c94aa9195e79
Eric Boesch <ericboesch@gmail.com>
2013-10-01 15:25:07 +0100

    ...

---
+++ nist/pededitor/PositionMenu.java
@@ -0,0 +1,41 @@
+package gov.nist.pededitor;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+/** Position submenu of the popup menu. */
+@SuppressWarnings("serial")
+public class PositionMenu extends JMenu {
+    Editor mEditor;
+
+    public PositionMenu(Editor editor) {
+        super("Position");
+        mEditor = editor;
+        System.out.println(mEditor.editFrame);
+        add(getEditFrame().actAutoPosition);
+        add(getEditFrame().actNearestPoint);
+        add(getEditFrame().actNearestCurve);
+        add(getEditFrame().actSelectNearestPoint);
+        add(getEditFrame().actSelectNearestCurve);
+    }
+
+
+    public Editor getParentEditor() {
+        return mEditor;
+    }
+
+    protected EditFrame getEditFrame() {
+        return getParentEditor().getEditFrame();
+    }
+
+    {
+        setMnemonic(KeyEvent.VK_P);
+    }
+
+    void add(AbstractAction act) {
+        add(new JMenuItem(act));
+    }
+}
