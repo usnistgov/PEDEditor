f28ba37bcdb55b7e35600247af55ccdbb9f5399f
Eric Boesch <ericboesch@gmail.com>
2013-10-01 17:39:50 +0100

    Now a subclass of BasicRightClickMenu

--- nist/pededitor/RightClickMenu.java
+++ nist/pededitor/RightClickMenu.java
@@ -1,17 +1,13 @@
 package gov.nist.pededitor;
 
-import javax.swing.JPopupMenu;
-import javax.swing.event.PopupMenuEvent;
-import javax.swing.event.PopupMenuListener;
 
-/** Popup menu that pops up when the user right-clicks on a point in
-    the diagram. */
+/** PED Editor popup menu. */
 @SuppressWarnings("serial")
-public class RightClickMenu extends JPopupMenu {
+public class RightClickMenu extends BasicRightClickMenu {
     Editor mEditor;
 
     public RightClickMenu(Editor editor) {
-        mEditor = editor;
+        super(editor);
         EditFrame ef = getEditFrame();
         add(ef.mnDeselect.getAction());
         addSeparator();
@@ -32,31 +28,5 @@ public class RightClickMenu extends JPopupMenu {
         addSeparator();
         add(ef.actCopyStatusBar);
         add(ef.actCenterMouse);
-
-        addPopupMenuListener(new PopupMenuListener() {
-                @Override public void popupMenuCanceled(PopupMenuEvent e) {
-                    System.out.println("Canceled."); // UNDO
-                    getEditor().rightClick = null;
-                }
-                @Override public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-                }
-                @Override public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
-                }
-            });
-    }
-
-    public void show(Editor.MousePress mp) {
-        if (getEditor().rightClick == null) {
-            getEditor().rightClick = mp;
-        }
-        show(mp.e.getComponent(), mp.e.getX(), mp.e.getY());
-    }
-
-    public Editor getEditor() {
-        return mEditor;
-    }
-
-    protected EditFrame getEditFrame() {
-        return getEditor().getEditFrame();
     }
 }
