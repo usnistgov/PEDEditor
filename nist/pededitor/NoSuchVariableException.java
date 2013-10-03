fdc0dcc517c9eebe83bc1b28d1874a509ed460b6
Eric Boesch <ericboesch@gmail.com>
2013-10-04 00:38:18 +0100

    ...

---
+++ nist/pededitor/NoSuchVariableException.java
@@ -0,0 +1,15 @@
+package gov.nist.pededitor;
+
+public class NoSuchVariableException extends Exception {
+    private static final long serialVersionUID = 5151827219327809828L;
+    String name;
+
+    public NoSuchVariableException(String name) {
+        super("No such variable '" + name + "'");
+	this.name = name;
+    }
+
+    String getVariableName() {
+        return name;
+    }
+}
