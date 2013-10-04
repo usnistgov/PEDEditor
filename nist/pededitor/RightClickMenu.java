5d60e323713a763a52554bb059fa865f232e4436
Eric Boesch <ericboesch@gmail.com>
2013-10-04 05:34:03 +0100

    Added setHasSelection() and setEditable()

--- nist/pededitor/RightClickMenu.java
+++ nist/pededitor/RightClickMenu.java
@@ -1,23 +1,29 @@
 package gov.nist.pededitor;
 
+import javax.swing.JMenu;
+
 
 /** PED Editor popup menu. */
 @SuppressWarnings("serial")
 public class RightClickMenu extends BasicRightClickMenu {
     Editor mEditor;
+    JMenu mnEdit = new JMenu("Edit Selection");
 
     public RightClickMenu(Editor editor) {
         super(editor);
         EditFrame ef = getEditFrame();
-        add(ef.mnDeselect.getAction());
+        add(ef.actDeselect);
+        mnEdit.setEnabled(false);
+        mnEdit.add(ef.actCopy);
+        mnEdit.add(ef.actCopyRegion);
+        mnEdit.add(ef.actMoveSelection);
+        mnEdit.add(ef.actMovePoint);
+        mnEdit.add(ef.actMoveRegion);
+        add(mnEdit);
         addSeparator();
         add(ef.mnJump);
         add(ef.mnMove);
         add(new PositionMenu(getEditor()));
-        addSeparator();
-        add(ef.actMoveSelection);
-        add(ef.actMovePoint);
-        add(ef.actMoveRegion);
         addSeparator();
         // If one does shift right-click, then the following two items
         // are identical, which can get a bit confusing.
@@ -30,5 +36,13 @@ public class RightClickMenu extends BasicRightClickMenu {
         addSeparator();
         add(ef.actCopyStatusBar);
         add(ef.actCenterMouse);
+    }
+
+    @Override public void setHasSelection(boolean b) {
+        mnEdit.setEnabled(b);
+    }
+
+    @Override public void setEditable(boolean b) {
+        mnEdit.setVisible(b);
     }
 }
