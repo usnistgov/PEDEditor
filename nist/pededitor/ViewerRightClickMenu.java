e7d6655d4a1c03f1ecb8acda2a86ea4f49843800
Eric Boesch <ericboesch@gmail.com>
2013-11-19 21:09:30 +0000

    Menu overhaul

--- nist/pededitor/ViewerRightClickMenu.java
+++ nist/pededitor/ViewerRightClickMenu.java
@@ -1,20 +1,30 @@
 package gov.nist.pededitor;
 
+import javax.swing.JLabel;
+
 
 /** PED Viewer popup menu. */
 @SuppressWarnings("serial")
 public class ViewerRightClickMenu extends BasicRightClickMenu {
     Editor mEditor;
+    JLabel coordinates;
 
     public ViewerRightClickMenu(Editor editor) {
         super(editor);
         EditFrame ef = getEditFrame();
-        add(ef.mnDeselect.getAction());
-        add(ef.mnJump);
-        add(ef.mnMove);
+        add(ef.actDeselect);
         add(getEditFrame().actSelectNearestPoint);
         add(getEditFrame().actSelectNearestCurve);
-        add(ef.actCopyStatusBar);
+        add(ef.mnJump);
+        add(ef.mnStep);
         add(ef.actCenterMouse);
+        addSeparator();
+        coordinates = createCoordinatesLabel();
+        add(ef.actCopyStatusBar);
+        add(coordinates);
+    }
+
+    @Override public void setCoordinates(String s) {
+        coordinates.setText(mungeCoordinates(s));
     }
 }
