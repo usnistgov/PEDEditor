a7f8b00dc2918c8d7495df050993c8344cb91d67
Eric Boesch <ericboesch@gmail.com>
2014-03-06 00:11:04 +0000

    Just a shell around NumberColumnDialog

--- nist/pededitor/DimensionsDialog.java
+++ nist/pededitor/DimensionsDialog.java
@@ -3,113 +3,41 @@
 
 package gov.nist.pededitor;
 
-import java.awt.BorderLayout;
-import java.awt.Dialog;
 import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 import java.util.Arrays;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JEditorPane;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 /** Dialog to ask for the length of two axes. */
-public class DimensionsDialog extends JDialog {
-    private static final long serialVersionUID = 2093995331175795482L;
+public class DimensionsDialog extends NumberColumnDialog {
 
-    boolean pressedOK = false;
-    JButton okButton;
-    JTextField[] textFields;
-    String[] dimensions = {"100.0", "100.0"};
+    private static final long serialVersionUID = -1831860481483477895L;
 
-    abstract class DimensionDialogAction extends AbstractAction {
-        private static final long serialVersionUID = 2659325739020932406L;
+    static final String intro = 
+"<p>Enter the fraction of the domain that the region you selected " +
+"covers. For example, for a lower right partial ternary diagram whose " +
+"bottom axis covers the range from 30% to 100% of the bottom right " +
+"component, enter 70% for the bottom side. " +
 
-        DimensionDialogAction(String name) {
-            super(name);
-        }
-    }
+"<p>If you wish to use different scales for the two axes, you may need " +
+"to enter the apparent axis length instead. For example, if the top " +
+"component in the diagram ranges from only 0-1%, but it " +
+"takes up as much room in the displayed image as if it ranged from " +
+"0-10%, then enter 10% for the axis length. Later, you can rescale the " +
+"Y axis (Properties/Scale/Y axis/top component) to " +
+"change the top Y value from 10% to the true value of 1%. " +
 
-    DimensionsDialog(Frame owner, String[] labels) {
-        super(owner, "Select Axis Lengths", false);
-
-        JPanel contentPane = (JPanel) getContentPane();
-
-        JPanel rows = new JPanel(new GridLayout(0, 2, 5, 0));
-        textFields = new JTextField[labels.length];
-
-        try {
-            JEditorPane textPane = new JEditorPane();
-            textPane.setEditable(false);
-            JEditorPanes.setHTMLResource
-                (textPane, "dimensionshelp.html", getClass());
-            contentPane.add(textPane, BorderLayout.PAGE_START);
-        } catch (IOException x) {
-            System.err.println(x);
-        }
-
-        for (int i = 0; i < labels.length; ++i) {
-            JLabel label = new JLabel(labels[i]);
-            JTextField text = new JTextField("100.0");
-            textFields[i] = text;
-            label.setLabelFor(text);
-            rows.add(label);
-            rows.add(text);
-        }
-        contentPane.add(rows, BorderLayout.CENTER);
-
-        okButton = new JButton(new DimensionDialogAction("OK") {
-                private static final long serialVersionUID = 2235207000149359195L;
-
-                @Override public void actionPerformed(ActionEvent e) {
-                    DimensionsDialog.this.pressedOK = true;
-                    setVisible(false);
-                }
-            });
-
-        contentPane.add(okButton, BorderLayout.PAGE_END);
-        getRootPane().setDefaultButton(okButton);
-    }
-
-    DimensionsDialog(Frame owner) {
-        this(owner, new String[] {"X Axis", "Y Axis"});
-    }
-
-    /** Set the dimensions strings. The user may change these values
-        (that's the point of presenting the dialog). */
-    public void setDimensions(String[] dimensions) {
-        for (int i = 0; i < dimensions.length; ++i) {
-            textFields[i].setText(dimensions[i]);
-        }
-    }
-
-    /** Show the dialog as document-modal, and return the selected
-        dimensions (as strings). Return null if the dialog was closed
-        abnormally. */
-    public String[] showModal() {
-        setModalityType(Dialog.ModalityType.DOCUMENT_MODAL);
-        pack();
-        setVisible(true);
-        if (pressedOK) {
-            String[] output = new String[textFields.length];
-            for (int i = 0; i < textFields.length; ++i) {
-                output[i] = textFields[i].getText();
-            }
-            return output;
-        } else {
-            return null;
-        }
+"<p>You may enter values as decimals, fractions, or percentages, but if " +
+"you use percentages, do not omit the percent sign.";
+    DimensionsDialog(Frame owner, double[] values, String[] labels) {
+        super(owner, values, labels, BasicEditor.htmlify(intro));
+        setTitle("Select Axis Lengths");
     }
 
     public static void main(String[] args) {
-        String[] dimensions = (new DimensionsDialog(null)).showModal();
-        System.out.println(Arrays.toString(dimensions));
+        DimensionsDialog dog = new DimensionsDialog
+            (null, new double[] {1, 0.5},  new String[] {"Right", "Bottom"});
+        dog.setPercentage(true);
+        double[] values = dog.showModalColumn();
+        System.out.println(Arrays.toString(values));
     }
    
 }
