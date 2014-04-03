b3123382e197b6e266ae0e83997a41dd28c74d59
Eric Boesch <ericboesch@gmail.com>
2014-04-03 20:24:22 +0100

    Added integrals

--- nist/pededitor/Polynomial.java
+++ nist/pededitor/Polynomial.java
@@ -16,13 +16,44 @@ public class Polynomial {
             return new double[0];
         }
 
-        double[] output = new double[degree];
+        double[] res = new double[degree];
 
         for (int i = 0; i < degree; ++i) {
-            output[i] = poly[i+1] * (i+1);
+            res[i] = poly[i+1] * (i+1);
         }
 
-        return output;
+        return res;
+    }
+
+    /** Return the integral of poly. */
+    public static double[] integral(double[] poly) {
+        int degree = degree(poly);
+        if (degree == -1) {
+            return new double[0];
+        }
+        double[] res = new double[degree + 2];
+
+        for (int i = 0; i <= degree; ++i) {
+            res[i+1] = poly[i] / (i+1);
+        }
+
+        return res;
+    }
+
+    /** Return the product of the two polynomials. */
+    public static double[] times(double[] poly1, double[] poly2) {
+        int d1 = degree(poly1);
+        int d2 = degree(poly2);
+        if (d1 < 0 || d2 < 0) {
+            return new double[0];
+        }
+        double[] res = new double[d1 + d2 + 1];
+        for (int i = 0; i <= d1; ++i) {
+            for (int j = 0; j <= d2; ++j) {
+                res[i+j] += poly1[i] * poly2[j];
+            }
+        }
+        return trim(res);
     }
 
     public static String toString(double[] poly) {
@@ -86,14 +117,28 @@ public class Polynomial {
         return result;
     }
 
+    /** Equivalent to evaluate(x, integral(poly)). */
+    public static double evaluateIntegral(double x, double[] poly) {
+        double result = 0;
+        for (int i = poly.length - 1; i >= 0; --i) {
+            result = (result + poly[i] / (i+1)) * x;
+        }
+        return result;
+    }
+
+    /** Evaluate the integral over the given range. */
+    public static double evaluateIntegral(double x1, double x2, double[] poly) {
+        return evaluateIntegral(x2, poly) - evaluateIntegral(x1, poly);
+    }
+
     public static double[] taylor(double x, double[] poly) {
-        double[] output = new double[poly.length];
+        double[] res = new double[poly.length];
         int degree = poly.length - 1;
         for (int i = 0; i <= degree; ++i) {
-            output[i] = evaluate(x, poly);
+            res[i] = evaluate(x, poly);
             poly = derivative(poly);
         }
-        return output;
+        return res;
     }
 
     /** Return the actual degree of this polynomial (ignoring zero
@@ -107,9 +152,26 @@ public class Polynomial {
         return -1;
     }
 
+    /** Remove all zero coefficients that do not have higher-order
+        nonzero coefficients and return the result (so for example
+        0x^2 + 5x + 0 becomes simply 5x + 0). This will simply return
+        its input (not even cloning it) unless poly[poly.length-1] ==
+        0. */
+    public static double[] trim(double[] poly) {
+        int d = degree(poly);
+        if (d == poly.length - 1) {
+            return poly;
+        }
+        double[] res = new double[d + 1];
+        for (int i = 0; i <= d; ++i) {
+            res[i] = poly[i];
+        }
+        return res;
+    }
+
     /** Return an array of the zeros of "poly" in order from least to
-        greatest. Currently works only on polynomials of degree 3 or
-        less. */
+        greatest. Currently throws IllegalArgumentException on
+        polynomials of degree 4 or greater. */
     public static double[] solve(double[] poly) {
         int d = degree(poly);
         if (d <= 0) {
@@ -150,7 +212,8 @@ public class Polynomial {
     }
 
     /** @return a 2-element array holding the minimum and maximum
-        values of the polynomial "poly" for t in [t0, t1]. */
+        values of the polynomial "poly" for t in [t0, t1]. Currently
+        throws IllegalArgumentException on polynomials of degree 5 or more. */
     public static double[] getBounds(double[] poly, double t0, double t1) {
         double v0 = evaluate(t0, poly);
         double v1 = evaluate(t1, poly);
