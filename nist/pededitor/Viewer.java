8659b59f0bc6dafb9337988443c18b90e7aded67
Eric Boesch <ericboesch@gmail.com>
2014-07-01 02:01:24 +0100

    Changes to support SingleInstanceBasicEditor etc.

--- nist/pededitor/Viewer.java
+++ nist/pededitor/Viewer.java
@@ -7,36 +7,17 @@
 package gov.nist.pededitor;
 
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 
 /** Main driver class for Phase Equilibria Diagram digitization and creation. */
-public class Viewer extends Editor {
-    static ArrayList<BasicEditor> openEditors = new ArrayList<>();
+public class Viewer extends SingleInstanceBasicEditor {
 
     public Viewer() {
-        openEditors.add(this);
+        init();
     }
 
-    void closeAll() {
-        // Duplicate openEditors so we don't end up iterating through a
-        // list that is simultaneously being modified.
-        for (BasicEditor e: new ArrayList<>(openEditors)) {
-            e.close();
-        }
-    }
-
-    @Override public void close() {
-        super.close();
-        openEditors.remove(this);
-    }
-
-    @SuppressWarnings("serial") protected void init() {
+    private void init() {
         alwaysConvertLabels = true;
         setRightClickMenu(new ViewerRightClickMenu(this));
         
@@ -44,7 +25,6 @@ public class Viewer extends Editor {
         EditFrame ef = editFrame;
         ef.setAlwaysOnTop(true);
         ef.setNewDiagramVisible(false);
-        ef.setOpenVisible(false);
         ef.setReloadVisible(false);
         ef.setEditable(false);
         ef.editingEnabled.setVisible(false);
@@ -56,30 +36,6 @@ public class Viewer extends Editor {
         ef.mnProperties.setVisible(false);
         ef.shortHelpFile = "viewhelp1.html";
         ef.helpAboutFile = "viewabout.html";
-        JMenuItem mnExitAll = new JMenuItem
-            (new Action("Exit all") {
-                    @Override public void actionPerformed(ActionEvent e) {
-                        closeAll();
-                    }
-                });
-        mnExitAll.setMnemonic(KeyEvent.VK_A);
-        ef.mnFile.add(mnExitAll);
-        ef.mnView.add(new Action("Hints..") {
-                @Override public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog
-                        (editFrame,
-                         htmlify
-                         ("<ol>"
-                          + "<li>You can see more functions and their short-cut keys "
-                          + "by right-clicking (pressing the right mouse button) "
-                          + " while the mouse is inside the diagram."
-                          + "<li>To zoom in, drag the mouse (move the mouse while holding down the left mouse button)."
-                          + "<li>Hold down the <code>Shift</code> key while moving the "
-                          + "mouse to find special points and curves, which will be "
-                          + "marked with a second pair of crosshairs."
-                          + "</ol>"));
-                }
-            });
 
         for (AbstractAction act: new AbstractAction[]
             { (AbstractAction) ef.mnUnstickMouse.getAction(),
@@ -138,10 +94,36 @@ public class Viewer extends Editor {
         ef.toFront();
     }
 
+    @Override void setFileAssociations() {
+        setFileAssociations
+            ("application/x-pedviewer", new String[] { "pedv" });
+    }
+
     @Override public void paintEditPane(Graphics g) {
         if (paintCnt == 0) {
             editFrame.setAlwaysOnTop(false);
         }
         super.paintEditPane(g);
+    }
+
+    @Override public void open() {
+        showOpenDialog(editFrame, openPEDFilesDialog(editFrame));
+    }
+
+    @Override public void run() {
+        open();
+    }
+
+    @Override String[] pedFileExtensions() {
+        return new String[] {"ped", "pedv"};
+    }
+
+    public static void main(String[] args) {
+        SingleInstanceBasicEditor.main
+            (new BasicEditorCreator() {
+                    @Override public BasicEditor run() {
+                        return new Viewer();
+                    }
+                }, args);
     }
 }
