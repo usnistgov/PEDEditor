a222180ce1cdbbbc44c3be7aee826c798e358729
Eric Boesch <ericboesch@gmail.com>
2014-09-24 22:37:11 +0100

    Added mathematical pi and e

--- nist/pededitor/HTMLPalette.java
+++ nist/pededitor/HTMLPalette.java
@@ -12,8 +12,11 @@ public class HTMLPalette extends StringPalette {
                     {"&gt;", ">"},
                     {"&amp;", "&"},
                     {"<br>\n", "<html><span style=\"font-size: 65%;\">line<br>break</span>"},
+                    {"\n<p>", "\u00b6" /* pilcrow paragraph symbol */ },
                     {"&nbsp;", "Hard space"},
-                }
+                },
+                "\ud835\udf0b" /* mathematical pi -- u+1d70b */,
+                "\ud835\udc52" /* mathematical e -- u+1d452 */
             });
     }
 }
