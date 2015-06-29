f6c79a05f5654cf3b11e75a781c24e22bfb05251
Eric Boesch <ericboesch@gmail.com>
2015-06-29 18:42:40 +0100

    Updated for newer version of iText. Make onlyShapes optional -- unfortunately it has to still be turned on because of an iText bug

--- nist/pededitor/DiagramPDF.java
+++ nist/pededitor/DiagramPDF.java
@@ -1,4 +1,4 @@
-/* Eric Boesch, NIST Materials Measurement Laboratory, 2014.
+ /* Eric Boesch, NIST Materials Measurement Laboratory, 2014.
  *
  * This file uses the iText library (http://itextpdf.com) and is
  * subject to the GNU Affero General Public License
@@ -10,30 +10,55 @@ import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import com.itextpdf.awt.DefaultFontMapper;
+import com.itextpdf.awt.PdfGraphics2D;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.DefaultFontMapper;
+import com.itextpdf.text.io.StreamUtil;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
 
 /** Class to add save-as-PDF capabilities to a Diagram. */
 public class DiagramPDF {
-    public static void saveAsPDF(Diagram d, File file) {
+    static class FontMap extends DefaultFontMapper {
+        Diagram d;
+        
+        FontMap(Diagram d) {
+            this.d = d;
+        }
+
+        @Override public BaseFont awtToPdf(java.awt.Font font) {
+            String filename = d.fontNameToFilename(font.getFontName());
+            if (filename == null) {
+                return super.awtToPdf(font);
+            } else {
+                return getItextFont(filename, font.getSize2D());
+            }
+        }
+    }
+
+    public static void saveAsPDF(Diagram d, File file) throws FileNotFoundException {
         saveAsPDF(d, new Document(PageSize.LETTER), file);
     }
 
-    public static void saveAsPDF(Diagram d, Document doc, File file) {
+    public static void saveAsPDF(Diagram d, Document doc, File file)
+        throws FileNotFoundException {
         PdfWriter writer = null;
         try {
             writer = PdfWriter.getInstance(doc, new FileOutputStream(file));
-        } catch (Exception e) {
-            System.err.println(e);
-            return;
+        } catch (DocumentException e) {
+            throw new FileNotFoundException();
         }
 
         doc.open();
@@ -64,6 +89,15 @@ public class DiagramPDF {
         return baos.toByteArray();
     }
 
+    static PdfGraphics2D createGraphics(Diagram d, PdfContentByte cb,
+                                        float w, float h, boolean onlyShapes) {
+        PdfGraphics2D res = onlyShapes ?
+            new PdfGraphics2D(cb, w, h, onlyShapes) :
+            new PdfGraphics2D(cb, w, h, new FontMap(d));
+        res.setFont(d.getFont());
+        return res;
+    }
+
     public static void appendToPDF(Diagram d, Document doc, PdfWriter writer) {
         String title = d.getTitle();
         if (title != null) {
@@ -81,16 +115,32 @@ public class DiagramPDF {
 
         PdfContentByte cb = writer.getDirectContent();
         PdfTemplate tp = cb.createTemplate((float) bounds.width, (float) bounds.height);
-        
-        @SuppressWarnings("unused")
-            Graphics2D g2 = false
-            ? tp.createGraphics((float) bounds.width, (float) bounds.height,
-                                new DefaultFontMapper())
-            : tp.createGraphicsShapes((float) bounds.width, (float) bounds.height);
 
-        g2.setFont(d.getFont());
+        // Use onlyShapes because of a bug in automatic boldtext in
+        // iText 5.5.6. That's too bad, because everything else about
+        // using real fonts seems to work. -- EB 6/29/2015
+        Graphics2D g2 = createGraphics
+            (d, cb, (float) bounds.width, (float) bounds.height, true);
         d.paintDiagram(g2, d.deviceScale(g2, bounds), null);
         g2.dispose();
         cb.addTemplate(tp, doc.left(), doc.bottom());
+    }
+
+    @JsonIgnore static public BaseFont getItextFont
+        (String filename, float font_size) {
+        InputStream is = (new DiagramPDF()).getClass()
+            .getResourceAsStream(filename);
+        if (is == null) {
+            throw new IllegalStateException
+                ("Could not locate font '" + filename + "'");
+        }
+        byte[] bytes;
+        try {
+            bytes = StreamUtil.inputStreamToArray(is);
+            return BaseFont.createFont(filename, BaseFont.IDENTITY_H, true, true,
+                                       bytes, null);
+        } catch (IOException | DocumentException x) {
+            throw new RuntimeException(x);
+        }
     }
 }
