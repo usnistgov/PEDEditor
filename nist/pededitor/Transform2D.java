24b9f2b007692ede249fdd7d5b9ab75ca9adab80
Eric Boesch <eric.boesch@nist.gov>
2016-08-16 01:16:04 -0400

    Moved defaults in from Transform2DAdapter.java

--- nist/pededitor/Transform2D.java
+++ nist/pededitor/Transform2D.java
@@ -13,33 +13,56 @@ interface Transform2D {
 
     /** Change this transformation into one that represents the
         composition of this transformation with the other
-        transformation. May not succeed, and should throw some kind of
-        exception in that case. */
-    void preConcatenate(Transform2D other);
+        transformation. */
+    default void preConcatenate(Transform2D other) {
+        throw new UnsupportedOperationException();
+    }
+    
 
     /** Change this transformation into one that represents the
         composition of the other transformation with this
-        transformation. May not succeed, and should throw some kind of
-        exception in that case. */
-   void concatenate(Transform2D other);
+        transformation. */
+    default void concatenate(Transform2D other) {
+        throw new UnsupportedOperationException();
+    }
 
     /** @return the transformation of (x,y) as a Point2D.Double */
     Point2D.Double transform(double x, double y) throws UnsolvableException;
-    Point2D.Double transform(Point2D.Double point) throws UnsolvableException;
-    Point2D.Double transform(Point2D point) throws UnsolvableException;
+
+    default Point2D.Double transform(Point2D.Double point)
+        throws UnsolvableException {
+        return transform(point.x, point.y);
+    }
+
+    default Point2D.Double transform(Point2D point)
+        throws UnsolvableException {
+        return transform(point.getX(), point.getY());
+    }
 
     /** Transform many points at once */
-    void transform(double[] srcPts, int srcOff,
-                   double[] dstPts, int dstOff, int numPts)
-        throws UnsolvableException;
+    default void transform(double[] srcPts, int srcOff,
+            double[] dstPts, int dstOff, int numPts)
+        throws UnsolvableException {
+        int twice = numPts * 2;
+        for (int i = 0; i < twice; i += 2) {
+            Point2D.Double outpt =
+                transform(srcPts[srcOff + i], srcPts[srcOff + i + 1]);
+            dstPts[dstOff + i] = outpt.x;
+            dstPts[dstOff + i + 1] = outpt.y;
+        }
+    }
 
     /** @return true if this transform never throws an
         UnsolvableException. */
-    boolean transformNeverThrows();
+    default boolean transformNeverThrows() {
+        return false; // Override otherwise
+    }
 
     /** @return true if this is an affine transformation of some
         kind. */
-    @JsonIgnore boolean isAffine();
+    @JsonIgnore default boolean isAffine() {
+        return false; // Override otherwise
+    }
 
     Transform2D clone();
 }
