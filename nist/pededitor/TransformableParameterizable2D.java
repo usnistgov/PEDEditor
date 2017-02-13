e7ae54b6bb723f94251ce22ead89cd8ae23756ff
Eric Boesch <eric.boesch@nist.gov>
2017-02-13 15:51:12 -0500

    simple interface

---
+++ nist/pededitor/TransformableParameterizable2D.java
@@ -0,0 +1,16 @@
+/* Eric Boesch, NIST Materials Measurement Laboratory, 2014. This file
+ * is placed into the public domain. */
+
+package gov.nist.pededitor;
+
+import java.awt.geom.AffineTransform;
+
+public interface TransformableParameterizable2D
+    extends BoundedParameterizable2D {
+    TransformableParameterizable2D createTransformed(AffineTransform xform);
+
+    /* This should return the parameterization of the transformed
+       control points, not the transform of the parameterization of
+       the original control points. */
+    BoundedParam2D getParameterization(AffineTransform xform);
+}
