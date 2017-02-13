c0b50744a36f5ff610747515319328580dda9c79
Eric Boesch <eric.boesch@nist.gov>
2017-02-13 16:51:01 -0500

    Decorated.java no longer used

--- nist/pededitor/Decorated.java
+++
@@ -1,8 +0,0 @@
-/* Eric Boesch, NIST Materials Measurement Laboratory, 2014. This file
- * is placed into the public domain. */
-
-package gov.nist.pededitor;
-
-// A hack to tell Jackson to include the "decoration" field in the serialization.
-public interface Decorated {
-}
