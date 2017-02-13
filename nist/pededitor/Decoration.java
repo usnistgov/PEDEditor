5dc234e9efaa181f56cad00c563f75763f98f753
Eric Boesch <eric.boesch@nist.gov>
2017-02-13 16:53:30 -0500

    Added default methods, transform(), createTransformed, requiredDecorations(), type parameter to getHandles(). Removed getMovementHandles.

--- nist/pededitor/Decoration.java
+++ nist/pededitor/Decoration.java
@@ -6,20 +6,25 @@ package gov.nist.pededitor;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface Decoration {
+interface Decoration {
+    default void draw(Graphics2D g) { draw(g, 1.0); };
     void draw(Graphics2D g, double scale);
-    /** @return the new SelectionHandle that should result from the removal
-        of this object, or null if the removal of this object should
-        leave the current selection empty. */
-    DecorationHandle remove();
-    void setLineWidth(double lineWidth);
-    double getLineWidth();
-    void setLineStyle(StandardStroke lineStyle);
-    StandardStroke getLineStyle();
+
+    default void draw(Graphics2D g, AffineTransform xform, double scale) {
+        createTransformed(xform).draw(g, scale);
+    }
+
+    default void setLineWidth(double lineWidth) {}
+    default double getLineWidth() { return 0; }
+    default void setLineStyle(StandardStroke lineStyle) {}
+    default StandardStroke getLineStyle() { return null; }
+    
     Color getColor();
     void setColor(Color color);
-    DecorationHandle[] getHandles();
+    DecorationHandle[] getHandles(DecorationHandle.Type type);
 
     /** Make the necessary changes to perform a reflection on this
         Decoration, EXCLUDING the transform of the movement handles.  */
@@ -29,26 +34,18 @@ public interface Decoration {
         the toPage transform is performed. Page Space is (x, y) where
         y increases as you go down the page. For example, the
         decoration might be changed so that upside down and backwards
-        text is put right right side up. */
+        text is put right side up. */
     default void neaten(AffineTransform toPage) {}
 
-    /* getMovementHandles() returns a subset of the handles of this
-     decoration that is sufficient to permit it to move. If this
-     decoration is defined in terms of positions on another
-     decoration, then the returned list should be empty, because
-     moving that other decoration will move this with it. If this
-     decoration has a fixed orientation but many potential grabbing
-     points, then the list should contain just one handle, since
-     moving that one will move the others along with it. */
-    DecorationHandle[] getMovementHandles();
+    /** createTransformed() transforms the control points (and
+        possibly angle for Angled decorations), which normally has a
+        different effect from just transforming the output image. */
+    void transform(AffineTransform xform);
 
-    /** Used during serialization. Some Decorations are just an
-        interface between an underlying object and the manipulating
-        environment, and in that case, serializing the underlying
-        object (as opposed to serializing the Decoration itself) is
-        enough (and if the Decoration is an inner class, then Jackson
-        can't deserialize the inner class directly anyhow). If that is
-        NOT the case, then it's perfectly legitimate just to have
-        getSerializationObject() return "this". */
-    Decorated getSerializationObject();
+    /** Like transform(), but creates a new object instead of overwriting this one. */
+    Decoration createTransformed(AffineTransform xform);
+
+    default List<Decoration> requiredDecorations() {
+        return new ArrayList<Decoration>();
+    }
 }
