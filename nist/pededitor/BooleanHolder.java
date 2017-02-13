2ea439783f9101ba2f2d7ad0e626bb3cbf71f539
Eric Boesch <eric.boesch@nist.gov>
2017-02-13 14:58:18 -0500

    trivial class

---
+++ nist/pededitor/BooleanHolder.java
@@ -0,0 +1,8 @@
+/* Eric Boesch, NIST Materials Measurement Laboratory, 2017. This file
+ * is placed into the public domain. */
+
+package gov.nist.pededitor;
+
+public class BooleanHolder {
+    boolean value;
+}
