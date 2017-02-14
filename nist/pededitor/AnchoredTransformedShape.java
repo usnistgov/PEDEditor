adf8b64e06235c70dc6f86a094eb6d1d8e1d3863
Eric Boesch <eric.boesch@nist.gov>
2017-02-13 19:53:08 -0500

    New superclass for AnchoredLabel

---
+++ nist/pededitor/AnchoredTransformedShape.java
@@ -0,0 +1,52 @@
+/* Eric Boesch, NIST Materials Measurement Laboratory, 2014. This file
+ * is placed into the public domain. */
+
+package gov.nist.pededitor;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+/** Class to hold a shape drawn at a given angle, scale, position,
+    with a given color, anchored at a specific location within the
+    shape's bounding box. */
+@JsonSerialize(include = Inclusion.NON_DEFAULT)
+abstract public class AnchoredTransformedShape extends TransformedShape {
+
+    /** Positioning relative to the anchor point. 0.0 = The
+        anchor point lies along the left edge of the shape in
+        baseline coordinates (if the shape is rotated, then this edge
+        may not be on the left in physical coordinates; for example,
+        if the shape is rotated by an angle of PI/2, then this will be
+        the top edge in physical coordinates); 0.5 = the anchor point
+        lies along the vertical line (in baseline coordinates) that
+        bisects the shape; 1.0 = the anchor point lies along the
+        right edge (in baseline coordinates) of the shape */
+    double xWeight;
+
+    /** Positioning relative to the anchor point. 0.0 = The
+        anchor point lies along the top edge of the shape in
+        baseline coordinates (if the shape is rotated, then this edge
+        may not be on top in physical coordinates; for example, if the
+        shape is rotated by an angle of PI/2, then this will be the
+        right edge in physical coordinates); 0.5 = the anchor point
+        lies along the horizontal line (in baseline coordinates) that
+        bisects the shape; 1.0 = the anchor point lies along the
+        bottom edge (in baseline coordinates) of the shape */
+    double yWeight;
+
+    public AnchoredTransformedShape() { }
+
+    public AnchoredTransformedShape(double xWeight, double yWeight) {
+        this.xWeight = xWeight;
+        this.yWeight = yWeight;
+    }
+
+    public void setXWeight(double xWeight) { this.xWeight = xWeight; }
+    public void setYWeight(double yWeight) { this.yWeight = yWeight; }
+    public double getXWeight() { return xWeight; }
+    public double getYWeight() { return yWeight; }
+
+    @Override public void reflect() {
+        setYWeight(1.0 - getYWeight());
+    }
+}
