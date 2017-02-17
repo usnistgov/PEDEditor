3ebaab11bfb3e2b0c5491b6130dd4595cde0cd3a
Eric Boesch <eric.boesch@nist.gov>
2017-02-17 18:08:56 -0500

    Basic version that does nothing.

---
+++ nist/pededitor/ImageHandle.java
@@ -0,0 +1,32 @@
+package gov.nist.pededitor;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
+public class ImageHandle implements DecorationHandle {
+
+    @Override
+    public DecorationHandle move(Point2D dest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DecorationHandle copy(Point2D dest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Decoration getDecoration() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
