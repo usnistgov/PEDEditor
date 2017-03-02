d8f12b96df7694c06234c7ec526c6ff5e11331b8
Eric Boesch <eric.boesch@nist.gov>
2017-03-02 13:56:20 -0500

    New clipboard stuff.

--- nist/pededitor/Stuff.java
+++ nist/pededitor/Stuff.java
@@ -6,7 +6,10 @@ package gov.nist.pededitor;
 import java.awt.Component;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
@@ -48,18 +51,49 @@ public class Stuff {
         JOptionPane.showMessageDialog
             (parent, htmlify(mess), title, JOptionPane.ERROR_MESSAGE);
     }
-    
-    /** Copy str to the clipboard. If it fails and ignoreFailure
-        equals false, throw IllegalArgumentException. */
-    public static void copyToClipboard(String str, boolean ignoreFailure) {
+
+    static String fakeClipboard = null;
+
+    /**
+     * Read the clipboard contents. If the clipboard cannot be read,
+     * but the fake clipboard that stores the result of failed
+     * attempts to write to the clipboard is not null, return that
+     * instead. */
+    static String getClipboardString() {
+        try {
+            return (String) Toolkit.getDefaultToolkit().getSystemClipboard()
+                .getData(DataFlavor.stringFlavor);
+        } catch (HeadlessException e) {
+            if (fakeClipboard != null)
+                return fakeClipboard;
+            throw new IllegalArgumentException(
+                    "Can't call coordinatesToClipboard() in a headless environment:"
+                    + e);
+        } catch (UnsupportedFlavorException e) {
+            throw new IllegalStateException
+                ("StringFlavor unsupported (internal:" + e);
+        } catch (IOException e) {
+            if (fakeClipboard != null)
+                return fakeClipboard;
+            throw new IllegalStateException
+                ("While getting clipboard: " + e);
+        }
+    }
+
+    /** Write str to the clipboard. If it fails and ignoreFailure
+        equals false, throw IllegalArgumentException. Also, on
+        failure, write to the fake clipboard so cut/paste can still
+        work. */
+    public static void setClipboardString(String str, boolean ignoreFailure) {
         try {
             StringSelection sel = new StringSelection(str);
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents
                 (sel, sel);
         } catch (HeadlessException e) {
+            fakeClipboard = str;
             if (!ignoreFailure) {
-                throw new IllegalArgumentException
-                    ("Can't call coordinatesToClipboard() in a headless environment:" + e);
+                throw new IllegalArgumentException(
+                        "Can't call coordinatesToClipboard() in a headless environment:" + e);
             }
         }
     }
@@ -69,7 +103,7 @@ public class Stuff {
     }
 
     static boolean fileAssociationBroken = false;
-    
+
     public static void setFileAssociationBroken(boolean b) {
         fileAssociationBroken = b;
     }
