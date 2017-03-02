07089c326d85d6b8691452151da1e591630e8595
Eric Boesch <eric.boesch@nist.gov>
2017-03-02 13:31:13 -0500

    move(dx,dy); transform(SlopeTransform2D)

--- nist/pededitor/Decoration.java
+++ nist/pededitor/Decoration.java
@@ -9,6 +9,8 @@ import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 interface Decoration {
     default void draw(Graphics2D g) { draw(g, 1.0); };
     void draw(Graphics2D g, double scale);
@@ -21,7 +23,7 @@ interface Decoration {
     default double getLineWidth() { return 0; }
     default void setLineStyle(StandardStroke lineStyle) {}
     default StandardStroke getLineStyle() { return null; }
-    
+
     Color getColor();
     void setColor(Color color);
     DecorationHandle[] getHandles(DecorationHandle.Type type);
@@ -37,22 +39,35 @@ interface Decoration {
         text is put right side up. */
     default void neaten(AffineTransform toPage) {}
 
-    /** createTransformed() transforms the control points (and
-        possibly angle for Angled decorations), which normally has a
-        different effect from just transforming the output image. */
+    void transform(SlopeTransform2D xform) throws UnsolvableException;
+
+    /**
+     * Transform the control points (and possibly angle for Angled decorations),
+     * which normally has a different effect from just transforming the output
+     * image.
+     */
     void transform(AffineTransform xform);
 
-    /** Like transform(), but creates a new object instead of overwriting this one. */
+    /** Like transform(), but create a new object. */
     Decoration createTransformed(AffineTransform xform);
 
     default List<Decoration> requiredDecorations() {
         return new ArrayList<Decoration>();
     }
 
+    /**
+     * Translate the position of this object by +(dx, dy).
+     * @param dx
+     * @param dy
+     */
+    default void move(double dx, double dy) {
+        transform(AffineTransform.getTranslateInstance(dx, dy));
+    }
+
     /** Return true if this decoration doesn't have enough points to
         display properly. Examples: a filled curve with zero area or a
         ruler with just one point. */
-    default boolean isDegenerate() { return false; }
+    default @JsonIgnore boolean isDegenerate() { return false; }
 
     Decoration clone();
 
