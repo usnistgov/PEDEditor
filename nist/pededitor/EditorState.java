8de58beb7dd52f5f39891126e376f212be30ea23
Eric Boesch <eric.boesch@nist.gov>
2017-03-10 14:07:56 -0500

    Save transient state for the user's convenience but don't count it when determining equality of two states. Retain save hash codes between restores.

--- nist/pededitor/EditorState.java
+++ nist/pededitor/EditorState.java
@@ -3,6 +3,7 @@
 
 package gov.nist.pededitor;
 
+import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +15,32 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 /** Stuff to help temporary storage of diagram state for do/undo. */
 class EditorState {
-    @JsonProperty Diagram diagram;
-    @JsonProperty int selectionDecorationNum = -1;
-    @JsonProperty int selectionHandleNum = -1;
+    private static Point2D.Double clone(Point2D.Double p) {
+        return (p == null) ? null : new Point2D.Double(p.x, p.y);
+    }
+
+    Diagram diagram;
+    TransientState trans;
+    static class TransientState {
+        int selectionDecorationNum = -1;
+        int selectionHandleNum = -1;
+        Point2D.Double mprin = null;
+        double scale = 0;
+
+        @Override public TransientState clone() {
+            TransientState res = new TransientState();
+            res.selectionDecorationNum = selectionDecorationNum;
+            res.selectionHandleNum = selectionHandleNum;
+            res.mprin = EditorState.clone(mprin);
+            res.scale = scale;
+            return res;
+        }
+    }
+
+    static class StringAndTransientState {
+        String str;
+        TransientState trans;
+    }
 
     /**
      * After verifying that the hash codes match, copy the image bytes
@@ -56,48 +80,68 @@ class EditorState {
         copyImagesBetween(target, this.diagram);
         DecorationsAndHandle wrap = new DecorationsAndHandle();
         wrap.decorations = diagram.decorations;
-        wrap.decorationNum = selectionDecorationNum;
-        wrap.handleNum = selectionHandleNum;
+        wrap.decorationNum = trans.selectionDecorationNum;
+        wrap.handleNum = trans.selectionHandleNum;
         DecorationHandle sel = wrap.createHandle();
 
+        // Preserve the target's undo stack and save hashes.
         int undoStackOffset = target.undoStackOffset;
-        ArrayList<String> undoStack = target.undoStack;
+        int lastSaveHashCode = target.lastSaveHashCode;
+        int autoSaveHashCode = target.autoSaveHashCode;
+        ArrayList<EditorState.StringAndTransientState> undoStack
+            = target.undoStack;
         try {
             target.undoStack = new ArrayList<>();
             target.copyFrom(this.diagram);
             target.setSelection(sel);
             target.revalidateZoomFrame();
+            target.lastSaveHashCode = lastSaveHashCode;
+            target.autoSaveHashCode = autoSaveHashCode;
+            // Restoring mprin and scale seems to be finicky.
+            // setScale() works, but it's worse than useless if the
+            // mouse is zoomed to the wrong spot.
+
+            // target.mprin = clone(trans.mprin);
+            // target.setScale( trans.scale;
+            // target.centerMouse();
         } finally {
             target.undoStack = undoStack;
             target.undoStackOffset = undoStackOffset;
         }
     }
 
-    /** @return this diagram as a JSON string. */
-    public String toJsonString() throws IOException {
-        return getObjectMapper().writeValueAsString(this);
+    public StringAndTransientState toStringAndTransientState()
+        throws IOException {
+        StringAndTransientState res = new StringAndTransientState();
+        res.str = getObjectMapper().writeValueAsString(diagram);
+        res.trans = trans.clone();
+        return res;
     }
 
     /** @return this diagram as a JSON string. */
-    public static String toJsonString(BasicEditor editor) throws IOException {
+    public static StringAndTransientState toStringAndTransientState(
+            BasicEditor editor) throws IOException {
         editor.resetIds();
         EditorState res = new EditorState();
         DecorationsAndHandle wrap = new DecorationsAndHandle();
         wrap.decorations = editor.decorations;
         wrap.saveHandle(editor.selection);
         res.diagram = editor;
-        res.selectionDecorationNum = wrap.decorationNum;
-        res.selectionHandleNum = wrap.handleNum;
-        return res.toJsonString();
+        res.trans = new TransientState();
+        res.trans.selectionDecorationNum = wrap.decorationNum;
+        res.trans.selectionHandleNum = wrap.handleNum;
+        res.trans.mprin = clone(editor.mprin);
+        res.trans.scale = editor.scale;
+        return res.toStringAndTransientState();
     }
 
-    static EditorState loadFrom(String jsonString) throws IOException {
-        EditorState res;
+    static EditorState loadFrom(StringAndTransientState sas) throws IOException {
+        EditorState res = new EditorState();
 
         try {
             ObjectMapper mapper = getObjectMapper();
-            res = (EditorState) mapper.readValue(jsonString, EditorState.class);
-            res.diagram.saveNeeded = true; // TODO be more sophisticated.
+            res.diagram = (Diagram) mapper.readValue(sas.str, Diagram.class);
+            res.trans = sas.trans.clone();
         } catch (Exception e) {
             throw new IOException("String parse error: " + e);
         }
@@ -106,9 +150,9 @@ class EditorState {
         return res;
     }
 
-    static void copyJsonStringToEditor(BasicEditor editor, String jsonString) throws IOException {
+    static void copyToEditor(BasicEditor editor, StringAndTransientState sas) throws IOException {
         String filename = editor.getFilename();
-        loadFrom(jsonString).copyTo(editor);
+        loadFrom(sas).copyTo(editor);
         editor.setFilename(filename);
     }
 
